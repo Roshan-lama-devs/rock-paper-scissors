0209d752c7073037c4a9cd7506050076b121c121
roshan-lama-devs <lamarosan1998@gmail.com>
2025-05-17 10:46:32 +1000

    Create function to get Human choice.

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
-console.log("Hello World");
-
+// Function that return Rock, paper or scissors randomly
 function getComputerChoice() {
   const randomNumber = Math.floor(Math.random() * 3);
   console.log(randomNumber);
@@ -11,5 +10,12 @@ function getComputerChoice() {
     return "Scissors";
   }
 }
-
 console.log(getComputerChoice());
+// function to get input from the user
+
+function getHumanChoice() {
+  let humanChoice = prompt("Please enter your choice");
+  console.log(humanChoice);
+}
+
+getHumanChoice();
