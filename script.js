c67c7150389e488fe9fc821c7065b84c41e48f94
roshan-lama-devs <lamarosan1998@gmail.com>
2025-05-17 10:40:54 +1000

    Generate random output for Rock,Paper,Scissors

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 console.log("Hello World");
+
+function getComputerChoice() {
+  const randomNumber = Math.floor(Math.random() * 3);
+  console.log(randomNumber);
+  if (randomNumber === 0) {
+    return "Rock";
+  } else if (randomNumber === 1) {
+    return "Paper";
+  } else {
+    return "Scissors";
+  }
+}
+
+console.log(getComputerChoice());
